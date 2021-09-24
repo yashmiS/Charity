752c4def77ad9fb4d8f456ddd4d25b793acc060f
thisuri-dileka <88765902+thisuri-dileka@users.noreply.github.com>
2021-09-25 01:06:25 +0530

    update login page

--- app/src/main/java/com/example/anonymouscharityapp/MainActivity.java
+++ app/src/main/java/com/example/anonymouscharityapp/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+//import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private Button button1;
 
     private TextView regpage;
 
@@ -17,8 +20,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        regpage = findViewById(R.id.regpage);
+//        button1= findViewById(R.id.blogbutton);
 
+        regpage = findViewById(R.id.regpage);
         regpage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(MainActivity.this,Blog.class);
+//                startActivity(intent);
+//
+//            }
+//        });
+
     }
 }
