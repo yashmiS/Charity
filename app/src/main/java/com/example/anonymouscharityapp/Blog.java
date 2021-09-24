2ac351d71f18c77274f44123179cd9d741d5807f
thisuri-dileka <88765902+thisuri-dileka@users.noreply.github.com>
2021-09-24 23:38:29 +0530

    create blog page

---
+++ app/src/main/java/com/example/anonymouscharityapp/Blog.java
@@ -0,0 +1,64 @@
+package com.example.anonymouscharityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class Blog extends AppCompatActivity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+
+    ViewFlipper flipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog);
+
+        button1= findViewById(R.id.voButton);
+        button2= findViewById(R.id.doButton3);
+        button3= findViewById(R.id.coButton2);
+        button4= findViewById(R.id.coeButton4);
+
+
+    flipper = (ViewFlipper) findViewById(R.id.flipper);
+
+    int imgarray[] = {R.drawable.animals,R.drawable.doggi,R.drawable.group,R.drawable.kids};
+
+        for (int i=0;i<imgarray.length;i++)
+    showimage(imgarray[i]);
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Blog.this,Donations.class);
+                startActivity(intent);
+
+            }
+        });
+}
+
+    public void showimage(int img){
+
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(img);
+
+        flipper.addView(imageView);
+        flipper.setFlipInterval(3000);
+        flipper.setAutoStart(true);
+
+        flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/anonymouscharityapp/Model.java
@@ -0,0 +1,17 @@
+package com.example.anonymouscharityapp;
+
+public class Model {
+    String name,amount,event;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+}
