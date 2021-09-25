bb1a92f9d1bd88ea39786165ca889bd8c78ec2ad
RoshanIT20210274 <86884584+RoshanIT20210274@users.noreply.github.com>
2021-09-25 15:05:49 +0530

    update pages

--- app/src/main/java/com/example/anonymouscharityapp/MainActivity.java
+++ app/src/main/java/com/example/anonymouscharityapp/MainActivity.java
@@ -91,7 +91,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void onComplete(@NonNull Task<AuthResult> task) {
                if(task.isSuccessful()){
                    //redirect to home page
-                   startActivity(new Intent(MainActivity.this, Profile.class));
+                   startActivity(new Intent(MainActivity.this,PaymentMethod.class));
                }else{
                    Toast.makeText(MainActivity.this, "Failed to Login", Toast.LENGTH_LONG).show();
                }
